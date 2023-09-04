f3f58f8852614d8de51c979516bbba70ad50004a
kimjiseung97 <dickseung23@naver.com>
2023-09-04 19:43:08 +0900

    # 환경 변수 설정및 출력 테스트

--- src/App.js
+++ src/App.js
@@ -5,6 +5,9 @@ import TodoList from './TodoList';
 
 
 function App() {
+  console.log(`REACT_APP_DATA_API= ${process.env.REACT_APP_DATA_API}`);
+  console.log(`REACT_APP_LOGIN_API= ${process.env.REACT_APP_LOGIN_API}`);
+
   return (
     <div className="App">
       <TodoList/>
