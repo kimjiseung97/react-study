6c574fddb903f3f952f1b4f7af7ddea42f6e484d
kimjiseung97 <dickseung23@naver.com>
2023-09-04 19:33:58 +0900

    # 테스트 및 빌드 테스트

--- src/util.test.js
+++ src/util.test.js
@@ -1,3 +1,8 @@
+//index.js
+import 'core-js/features/string/pad-start';
+
+//someFile.js
+const value = '123'.padStart(5,'0'); // 00123
 import { addNumber } from "./util";
 
 it('add two numbers', ()=>{
